b319d4194ea713a4bf4ae11c8c8bc38546f5816a
Elif Atci <eelif0600@gmail.com>
2023-11-19 22:08:56 +0300

    191123

--- src/main/java/maven_1/T2.java
+++ src/main/java/maven_1/T2.java
@@ -1,13 +1,47 @@
 package maven_1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class T2 {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
         //3. Verify that home page is visible successfully
+        WebElement homePage = driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
+        if (homePage.isDisplayed()) {
+            System.out.println("HomePage test is PASSED");
+        } else {
+            System.out.println("HomePage test is FAİLED");
+        }
         //4. Click on 'Signup / Login' button
+        driver.findElement(By.xpath("//a[@href='/login']")).click();
         //5. Verify 'New User Signup!' is visible
+        WebElement newUser = driver.findElement(By.xpath("//h2[text()='New User Signup!']"));
+        if (newUser.isDisplayed()) {
+            System.out.println("newUser test is PASSED");
+        } else {
+            System.out.println("newUser test is FAİLED");
+        }
         //6. Enter name and already registered email address
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("elif");
+        driver.findElement(By.xpath("(//input[@name='email'])[2]")).sendKeys("ee@dd.com");
         //7. Click 'Signup' button
+        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
         //8. Verify error 'Email Address already exist!' is visible
+        WebElement alreadyExist = driver.findElement(By.xpath("//p[text()='Email Address already exist!']"));
+        if (alreadyExist.isDisplayed()) {
+            System.out.println("alreadyExist test is PASSED");
+        } else {
+            System.out.println("alreadyExist test is FAİLED");
+        }
+        driver.quit();
     }
 }
---
+++ src/main/java/maven_1/T3.java
@@ -0,0 +1,65 @@
+package maven_1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class T3 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        //3. Verify that home page is visible successfully
+        WebElement homePage = driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
+        if (homePage.isDisplayed()) {
+            System.out.println("HomePage test is PASSED");
+        } else {
+            System.out.println("HomePage test is FAİLED");
+        }
+        //4. Click on 'Contact Us' button
+        driver.findElement(By.xpath("//a[@href='/contact_us']")).click();
+        //5. Verify 'GET IN TOUCH' is visible
+        WebElement getinTouch = driver.findElement(By.xpath("(//h2[@class='title text-center'])[2]"));
+        if (getinTouch.isDisplayed()) {
+            System.out.println("getinTouch test is PASSED");
+        } else {
+            System.out.println("getinTouch test is FAİLED");
+        }
+        //6. Enter name, email, subject and message
+        driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys("elif");
+        driver.findElement(By.xpath("(//input[@type='email'])[1]")).sendKeys("ee@dd.com");
+        driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys("giris sorunu");
+        driver.findElement(By.xpath("//textarea[@id='message']")).sendKeys("giris yapamiyorum");
+        //7. Upload file
+        WebElement file = driver.findElement(By.xpath("//input[@name='upload_file']"));
+        file.sendKeys("C:/Users/Elif/OneDrive/Masaüstü/el.png");
+
+
+        //8. Click 'Submit' button
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        //9. Click OK button
+        Thread.sleep(4000);
+        //10. Verify success message 'Success! Your details have been submitted successfully.' is visible
+        WebElement success = driver.findElement(By.xpath("//div[@class='status alert alert-success']"));
+        if (success.isDisplayed()) {
+            System.out.println("Success test is PASSED");
+        } else {
+            System.out.println("Success test is FAİLED");
+        }
+        //11. Click 'Home' button and verify that landed to home page successfully
+        WebElement home = driver.findElement(By.xpath("//span[text()=' Home']"));
+        home.click();
+        homePage = driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
+        if (homePage.isDisplayed()) {
+            System.out.println("HomePage test is PASSED");
+        } else {
+            System.out.println("HomePage test is FAİLED");
+        }
+        driver.quit();
+    }
+}
