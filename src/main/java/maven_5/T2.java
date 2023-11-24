5c2ea0a4d5e163c0a94302937da3fa4b69a3a780
Elif Atci <eelif0600@gmail.com>
2023-11-24 22:58:26 +0300

    241123

---
+++ src/main/java/maven_5/T2.java
@@ -0,0 +1,30 @@
+package maven_5;
+
+import Methods.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class T2 extends TestBase {
+    @Test
+    public void test01() {
+        // Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
+        // a. Verilen web sayfasına gidin.
+        // https://facebook.com
+        driver.get("https://facebook.com");
+        // 	b. Cookies’i kabul edin
+        // 	c. Create an account buton’una basin
+        driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
+        // 	d. Radio button elementlerini locate edin ve size uygun olani secin
+        WebElement kadinRadioButton = driver.findElement(By.xpath("(//input[@class='_8esa'])[1]"));
+        kadinRadioButton.click();
+        WebElement erkekRadioButton = driver.findElement(By.xpath("(//input[@class='_8esa'])[2]"));
+        WebElement digerRadioButton = driver.findElement(By.xpath("(//input[@class='_8esa'])[3]"));
+        // 	e. Sectiginiz radio button’un seçili, ötekilerin seçili olmadigini test edin
+        Assert.assertTrue(kadinRadioButton.isSelected());
+        Assert.assertFalse(erkekRadioButton.isSelected());
+        Assert.assertFalse(digerRadioButton.isSelected());
+    }
+
+}
