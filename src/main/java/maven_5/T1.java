a4732ef6a9b30017c2afd9b4e8d2ad3f7ec52f48
Elif Atci <eelif0600@gmail.com>
2023-11-24 21:34:03 +0300

    241123

---
+++ src/main/java/maven_5/T1.java
@@ -0,0 +1,43 @@
+package maven_5;
+
+import Methods.ReusableMethods;
+import Methods.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class T1 extends TestBase {
+    @Test
+    public void exercise13() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        //3. Verify that home page is visible successfully
+        WebElement homePage = driver.findElement(By.id("slider-carousel"));
+        Assert.assertTrue(homePage.isDisplayed());
+        //4. Click 'View Product' for any product on home page
+        WebElement view = driver.findElement(By.xpath("(//a[text()='View Product'])[1]"));
+        js.executeScript("arguments[0].click();", view);
+
+        //5. Verify product detail is opened
+        WebElement detail = driver.findElement(By.className("product-information"));
+        Assert.assertTrue(detail.isDisplayed());
+        //6. Increase quantity to 4
+        WebElement quantity = driver.findElement(By.id("quantity"));
+        quantity.clear();
+        quantity.sendKeys("4");
+        //7. Click 'Add to cart' button
+        driver.findElement(By.xpath("//button[@type='button']")).click();
+        //8. Click 'View Cart' button
+        driver.findElement(By.xpath("//u[text()='View Cart']")).click();
+        //9. Verify that product is displayed in cart page with exact quantity
+        WebElement result = driver.findElement(By.className("disabled"));
+        int expected = 4;
+        String actualStr = result.getText();
+        int actual = Integer.parseInt(actualStr);
+        Assert.assertEquals(expected, actual);
+    }
+}
---
+++ src/main/java/maven_5/exercise3.java
@@ -0,0 +1,53 @@
+package maven_5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class exercise3 {
+ static WebDriver driver;
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @After
+    public void teardown(){
+        driver.close();
+    }
+    @Test
+    public void exercise3(){
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        //3. Verify that home page is visible successfully
+        WebElement homePage=driver.findElement(By.id("slider-carousel"));
+      Assert.assertTrue(homePage.isDisplayed());
+        //4. Click on 'Signup / Login' button
+        driver.findElement(By.xpath("//a[@href='/login']")).click();
+        //5. Verify 'Login to your account' is visible
+        WebElement login=driver.findElement(By.className("login-form"));
+        Assert.assertTrue(login.isDisplayed());
+        //6. Enter incorrect email address and password
+        driver.findElement(By.xpath("(//input[@type='email'])[1]")).sendKeys("ee@ee.com");
+        driver.findElement(By.xpath("(//input[@type='password'])[1]")).sendKeys("1234567890");
+        //7. Click 'login' button
+        driver.findElement(By.xpath("(//button[@type='submit'])[1]")).click();
+        //8. Verify error 'Your email or password is incorrect!' is visible
+        WebElement error=driver.findElement(By.xpath("//p[@style='color: red;']"));
+        String expectedMessage="Your email or password is incorrect!";
+        String actualMessage=error.getText();
+        Assert.assertEquals(expectedMessage,actualMessage);
+    }
+
+}
