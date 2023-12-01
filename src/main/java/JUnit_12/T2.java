1a447148f9bc43ae536a6eca790974f84dd3a8a1
Elif Atci <eelif0600@gmail.com>
2023-12-01 23:03:12 +0300

    011223

--- src/main/java/JUnit_12/T2.java
+++ src/main/java/JUnit_12/T2.java
@@ -1,15 +1,57 @@
 package JUnit_12;
 
-public class T2 {
-    // https://trytestingthis.netlify.app/ adresine gidin
-    // This is your form title bolumunde bulunan
-    //      First name:
-    //      Last name:
-    //      Gender(Kendinize uygun olani) doldurun
-    // Choose an option bolumunde bulunan
-    //   Lets you select only one optionda Option2 yi secerek
-    //      Option2 nin secili digerlerinin secili olmadigini dogrulayin
-    // Lets you select multiple options bolumunde bulunan
-    //      Option ve Option4 u secip digerlerini secmeden
-    //          secili olanlari ve olmayanlari dogrulayin
+import Methods.ReusableMethods;
+import Methods.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class T2 extends TestBase {
+
+    @Test
+    public void test01(){
+        // https://trytestingthis.netlify.app/ adresine gidin
+        driver.get("https://trytestingthis.netlify.app/");
+        // This is your form title bolumunde bulunan
+        //      First name:
+        //      Last name:
+        //      Gender(Kendinize uygun olani) doldurun
+        Actions actions=new Actions(driver);
+        WebElement firstNameBox= driver.findElement(By.id("fname"));
+        actions.click(firstNameBox).sendKeys("elif")
+                .sendKeys(Keys.TAB)
+                .sendKeys("elif")
+                .perform();
+        driver.findElement(By.xpath("//label[@for='female']")).click();
+        // Choose an option bolumunde bulunan
+        //   Lets you select only one optionda Option2 yi secerek
+        //      Option2 nin secili digerlerinin secili olmadigini dogrulayin
+        WebElement dropdownBox= driver.findElement(By.xpath("//select[@name='option']"));
+        Select select=new Select(dropdownBox);
+        select.selectByVisibleText("Option 2");
+        WebElement option= driver.findElement(By.xpath("(//option[@value='option'])[1]"));
+        WebElement option1= driver.findElement(By.xpath("(//option[@value='option 1'])[1]"));
+        WebElement option2= driver.findElement(By.xpath("(//option[@value='option 2'])[1]"));
+        WebElement option3= driver.findElement(By.xpath("(//option[@value='option 3'])[1]"));
+        Assert.assertTrue(option2.isSelected());
+        Assert.assertFalse(option.isSelected()&&option1.isSelected()&&option3.isSelected());
+
+        // Lets you select multiple options bolumunde bulunan
+        //      Option ve Option4 u secip digerlerini secmeden
+        //          secili olanlari ve olmayanlari dogrulayin
+       option= driver.findElement(By.xpath("(//option[@value='option'])[2]"));
+       option1= driver.findElement(By.xpath("(//option[@value='option 1'])[2]"));
+       option2= driver.findElement(By.xpath("(//option[@value='option 2'])[2]"));
+       option3= driver.findElement(By.xpath("(//option[@value='option 3'])[2]"));
+
+       actions.keyDown(Keys.CONTROL).click(option).click(option3).perform();
+       Assert.assertTrue(option.isSelected()&&option3.isSelected());
+       Assert.assertFalse(option1.isSelected()&&option2.isSelected());
+
+    }
+
 }
--- src/main/java/JUnit_12/T3.java
+++ src/main/java/JUnit_12/T3.java
@@ -1,19 +1,67 @@
 package JUnit_12;
 
-public class T3 {
-    // https://trytestingthis.netlify.app/ adresine gidin
-    // Choose an option bolumunde bulunan
-    // This is your form title bolumunde bulunan
-    //      Choose applicable options bolumunde
-    //         Option2 ve Option3 u secin, bunlarin secili
-    //              ve Option1 in secili olmadigini dogrulayin
-    // Start typing and it till automatically guess answer bolumune
-    //      tahmini cevabinizi yazin
-    // Select your favorite color renginizi secin
-    // Select a date bolumunden bir tarih secin
-    // Scroll to see a range value ile "66" yi secin ve birakin
-    // Select a file bolumune bir dosya upload edin ve dogrulayin
-    // Select a quantity from a range' i 4' e getirin ve dogrulayin
-    // Long Message bolumune bir metin girin
-    // Submit yapin
+import Methods.ReusableMethods;
+import Methods.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class T3 extends TestBase {
+
+    @Test
+    public void test01(){
+        JavascriptExecutor js=(JavascriptExecutor) driver;
+        // https://trytestingthis.netlify.app/ adresine gidin
+        driver.get("https://trytestingthis.netlify.app/");
+        // Choose an option bolumunde bulunan
+        // This is your form title bolumunde bulunan
+        //      Choose applicable options bolumunde
+        //         Option2 ve Option3 u secin, bunlarin secili
+        //              ve Option1 in secili olmadigini dogrulayin
+        WebElement option1= driver.findElement(By.xpath("(//input[@id='moption'])[1]"));
+        WebElement option2= driver.findElement(By.xpath("(//input[@id='moption'])[2]"));
+        WebElement option3= driver.findElement(By.xpath("(//input[@id='moption'])[3]"));
+        Actions actions=new Actions(driver);
+        actions.click(option2).click(option3).perform();
+        Assert.assertTrue(option2.isSelected()&&option3.isSelected());
+        Assert.assertFalse(option1.isSelected());
+        ReusableMethods.Wait(3);
+
+        // Start typing and it till automatically guess answer bolumune
+        //      tahmini cevabinizi yazin
+
+        driver.findElement(By.xpath("//input[@list='datalists']")).sendKeys("Chocolate");
+        //js.executeScript("arguments[0].scrollIntoView(true);",dropDownBox);
+
+        // Select your favorite color renginizi secin
+
+        // Select a date bolumunden bir tarih secin
+        driver.findElement(By.xpath("//input[@type='date']")).sendKeys("20091993");
+        // Scroll to see a range value ile "66" yi secin ve birakin
+        WebElement scrollValue= driver.findElement(By.xpath("//input[@type='range']"));
+       // WebElement fieldValue= driver.findElement(By.xpath("//output[text()='66']"));
+        //actions.dragAndDrop(scrollValue,fieldValue).perform();
+        // Select a file bolumune bir dosya upload edin ve dogrulayin
+       WebElement fileBox= driver.findElement(By.xpath("//input[@type='file']"));
+        js.executeScript("arguments[0].click();",fileBox);
+       fileBox.sendKeys("C:\\Users\\Elif\\Downloads\\logo.png");
+        ReusableMethods.Wait(3);
+        // Select a quantity from a range' i 4' e getirin ve dogrulayin
+        driver.findElement(By.id("quantity")).sendKeys("4");
+        ReusableMethods.Wait(3);
+        // Long Message bolumune bir metin girin
+       WebElement message= driver.findElement(By.name("message"));
+       message.clear();
+       message.sendKeys("happy weekend");
+        ReusableMethods.Wait(9);
+        // Submit yapin
+        driver.findElement(By.xpath("//button[@class='btn btn-success']")).click();
+        ReusableMethods.Wait(3);
+    }
+
 }
